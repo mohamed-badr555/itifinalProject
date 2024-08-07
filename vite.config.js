53211517600254019a7ef6063ef13c00ee9b49ae
mohamed-badr555 <mohamedb.555dr@gmail.com>
2024-08-08 01:48:29 +0300

    Update base8

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/itifinalProject/', // This should match your repository name
-});
+  base:"/itifinalProject/"
+})
